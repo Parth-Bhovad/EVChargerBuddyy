14484732c5d3b0c06f213b209fea708d71a2c969
Parth Bhovad <parthbhovad710@gmail.com>
2025-12-02 12:34:06 +0530

    middleware/proxy added

---
+++ src/proxy.ts
@@ -0,0 +1,20 @@
+// middleware.ts
+import { auth } from "@/app/lib/auth"; // your betterAuth instance
+import { NextResponse } from "next/server";
+
+export async function proxy(req: Request) {
+  const session = await auth.api.getSession({ headers: req.headers });
+
+  if (!session) {
+    // redirect to login if no session
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  // allow request if session exists
+  return NextResponse.next();
+}
+
+// Protect specific routes
+export const config = {
+  matcher: ["/profile"], // routes to protect
+};
