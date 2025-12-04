a83c6035be3bf28e38830b8316b495b2b111d2f0
Parth Bhovad <parthbhovad710@gmail.com>
2025-12-04 23:31:17 +0530

    Profile UI Component UI Improved

--- src/app/ui/Profile.tsx
+++ src/app/ui/Profile.tsx
@@ -4,21 +4,27 @@ import Link from "next/link";
 import { LogoutUser } from "../lib/action";
 import Button from "../ui/Button";
 
-function Profile({name}: {name: string | undefined}) {
+function Profile({ name }: { name: string | undefined }) {
     return (
-        <main className="h-screen">
-            <section>
-                <div>
-                    Username: {name}
+        <>
+            <div className="avatar avatar-placeholder">
+                <div className="bg-neutral text-neutral-content w-20 rounded-full">
+                    <span className="text-3xl">{name?.charAt(0).toUpperCase()}</span>
                 </div>
-            </section>
-            <div>
-                <Link href="/charging-station">Add Charging Station</Link>
             </div>
-            <div>
-                <Button btnName="Logout" onClick={LogoutUser} />
+            <div className="text-2xl">
+                {name}
             </div>
-        </main>
+
+            <ul className="list rounded-box shadow-md w-full mt-8 border border-base-300">
+                <li className="list-row rounded-none border-b border-base-300 flex justify-between items-center">
+                    <Link href="/charging-station">Add Charging Station</Link>
+                </li>
+                <li className="list-row">
+                    <Button btnName="Logout" onClick={LogoutUser} />
+                </li>
+            </ul>
+        </>
     );
 }
 
