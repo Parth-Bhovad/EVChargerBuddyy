161505c03ab8f6186197144a5378195d5b6059e2
Parth Bhovad <parthbhovad710@gmail.com>
2025-12-04 23:31:39 +0530

    SIgnup UI improved

--- src/app/ui/Signup.tsx
+++ src/app/ui/Signup.tsx
@@ -4,29 +4,31 @@ import UserNameInput from "./UserNameInput";
 import EmailInput from "./EmailInput";
 import PasswordInput from "./PasswordInput";
 import Button from "./Button";
-import {SignupUser} from "../lib/action";
+import { SignupUser } from "../lib/action";
 
 function Signup() {
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     return (
-        <form action={SignupUser} className="grid">
-            <div className="my-2">
-                <UserNameInput username={username} setUsername={setUsername} />
-            </div>
-            <div className="my-2">
-                <EmailInput email={email} setEmail={setEmail} />
-            </div>
-            <div className="my-2">
-                <PasswordInput password={password} setPassword={setPassword} />
-            </div>
+        <fieldset className="fieldset border-base-300 rounded-box w-xs border p-4">
+            <form action={SignupUser} className="grid">
+                <div className="my-2">
+                    <UserNameInput username={username} setUsername={setUsername} />
+                </div>
+                <div className="my-2">
+                    <EmailInput email={email} setEmail={setEmail} />
+                </div>
+                <div className="my-2">
+                    <PasswordInput password={password} setPassword={setPassword} />
+                </div>
 
-            <div className="my-4">
-                <Button btnName="Sign Up" btnType="submit" />
-            </div>
-        </form>
-     );
+                <div className="my-4">
+                    <Button btnName="Sign Up" btnType="submit" />
+                </div>
+            </form>
+        </fieldset>
+    );
 }
 
 export default Signup;
