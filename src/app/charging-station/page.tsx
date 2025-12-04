f9c243b10355fd562a36b0116b896ddf5374fac0
Parth Bhovad <parthbhovad710@gmail.com>
2025-12-04 23:20:00 +0530

    UI Improvement

--- src/app/charging-station/page.tsx
+++ src/app/charging-station/page.tsx
@@ -1,7 +1,12 @@
 import ChargingStation from "@/app/ui/ChargingStation";
 
 function ChargingStationPage() {
-    return ( <ChargingStation /> );
+    return (
+        <main className="max-w-[520px] mx-auto mt-12 p-5 h-screen flex flex-col items-center">
+            <h1 className="text-2xl font-semibold mb-3 text-[var(--brand-color-black)] text-center">List Your Charging Station</h1>
+            <ChargingStation />
+        </main>
+    );
 }
 
 export default ChargingStationPage;
