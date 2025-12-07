181e70d50617f68776ef2265dad3df0b3031f037
Parth Bhovad <parthbhovad710@gmail.com>
2025-12-07 19:14:52 +0530

    The Start position is not the users current live location. Implemented this using React Context API

--- src/app/ui/Map.tsx
+++ src/app/ui/Map.tsx
@@ -2,7 +2,32 @@
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
 import L from 'leaflet';
+import { useState } from 'react';
+import { useMapEvents } from 'react-leaflet/hooks'
+import RouteMachine from './RouteMachine';
+import { useUserLocationContext } from '../context/UserLocationContext';
+
+function LocationMarker() {
+  // const [position, setPosition] = useState(null)
+  const { userLocation, setUserLocation } = useUserLocationContext();
+  const map = useMapEvents({
+    click() {
+      map.locate()
+    },
+    locationfound(e) {
+      setUserLocation([e.latlng.lat, e.latlng.lng])
+      map.flyTo(e.latlng, map.getZoom())
+    },
+  })
+
+  return userLocation === null ? null : (
+    <Marker position={userLocation}>
+      <Popup>You are here</Popup>
+    </Marker>
+  )
+}
 
 L.Icon.Default.mergeOptions({
   iconRetinaUrl:
@@ -13,10 +38,10 @@ L.Icon.Default.mergeOptions({
     'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
 });
 
-const LeafletMap = ({stations}) => {
+const LeafletMap = ({ stations }) => {
   const position: [number, number] = [19.6967, 72.7655]; // Palghar
-  console.log("Map:", stations);
-  
+  const { userLocation } = useUserLocationContext();
+  const [destination, setDestination] = useState<[number, number] | null>(null);
   return (
     <MapContainer
       center={position}
@@ -30,12 +55,23 @@ const LeafletMap = ({stations}) => {
       />
       {stations.map((station, index) => (
         <Marker
-        key={index}
-        position={[station[0], station[1]]}
-      >
-        <Popup>{index}</Popup>
-      </Marker>
+          key={index}
+          position={[station[0], station[1]]}
+          eventHandlers={
+            {
+              click: (e) => {
+                console.log(e);
+                setDestination([e.latlng.lat, e.latlng.lng]);
+              }
+
+            }
+          }
+        >
+          <Popup>{index}</Popup>
+        </Marker>
       ))}
+      <LocationMarker />
+      {destination && userLocation && <RouteMachine from={userLocation} to={destination} />}
     </MapContainer>
   );
 };
