012f85447cbbea457c2d8b51a714b2aac97ee115
Parth Bhovad <parthbhovad710@gmail.com>
2025-12-07 19:19:24 +0530

    useUserLocationContext Added

---
+++ src/app/context/UserLocationContext.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { createContext, useState, useContext } from 'react';
+
+const UserLocationContext = createContext<{
+    userLocation: [number, number] | null;
+    setUserLocation: React.Dispatch<React.SetStateAction<[number, number] | null>>;
+} | null>(null);
+
+export const useUserLocationContext = () => {
+    const context = useContext(UserLocationContext);
+    if (context === undefined) {
+        throw new Error('useUserLocation must be used within a UserLocationProvider');
+    }
+    return context;
+}
+
+function UserLocationProvider({ children }: { children: React.ReactNode }) {
+    const [userLocation, setUserLocation] = useState<[number, number] | null>(null)
+    return ( 
+        <UserLocationContext.Provider value={{ userLocation, setUserLocation }}>
+            {children}
+        </UserLocationContext.Provider>
+     );
+}
+
+export default UserLocationProvider;
