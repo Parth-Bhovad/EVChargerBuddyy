88b1c9661ad0b9d6d9dbb4ac932ab3771a10a8aa
Parth Bhovad <parthbhovad710@gmail.com>
2025-12-10 16:35:41 +0530

    useState props removed

--- src/app/ui/StationNameInput.tsx
+++ src/app/ui/StationNameInput.tsx
@@ -1,12 +1,12 @@
-function StationNameInput({ stationName, setStationName }: { stationName: string, setStationName: (stationName: string) => void }) {
+function StationNameInput() {
     return (
         <>
             <input
                 type="text"
                 placeholder="Station Name (optional)"
                 className="input bg-[var(--brand-color-white)] text-[var(--brand-color-black)] border border-base-300 rounded-box"
-                value={stationName}
-                onChange={(e) => setStationName(e.target.value)} />
+                name="stationName"
+            />
         </>
     );
 }
