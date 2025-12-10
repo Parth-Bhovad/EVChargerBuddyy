4a923c4aeed6cc7fc76b4aec979565a2e57d8338
Parth Bhovad <parthbhovad710@gmail.com>
2025-12-10 16:30:51 +0530

    Loading state added in  buttons

--- src/app/ui/Button.tsx
+++ src/app/ui/Button.tsx
@@ -1,12 +1,22 @@
+import Loading from "./Loading";
+
 interface ButtonProps {
     btnName: string;
     btnType?: "button" | "submit" | "reset";
-    onClick?: (() => Promise<void>)| (() => void);
+    onClick?: (() => Promise<void>) | (() => void);
+    loading?: boolean;
 }
 
-function Button({btnName, btnType = "button", onClick}: ButtonProps) {
+function Button({ btnName, btnType = "button", onClick, loading = false }: ButtonProps) {
     return (
-        <button type={btnType} className="btn btn-block rounded-box font-bold text-[var(--brand-color-white)]" onClick={onClick}>{btnName}</button>
+        <button
+            type={btnType}
+            className="btn btn-block rounded-box font-bold text-[var(--brand-color-white)]"
+            onClick={onClick}
+            >
+
+            {loading ? <Loading /> : btnName}
+        </button>
     );
 }
 
