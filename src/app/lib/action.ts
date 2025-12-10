439c1708561d719b6b180de87307146489248b1e
Parth Bhovad <parthbhovad710@gmail.com>
2025-12-10 16:28:58 +0530

    Error Handling added in Server functions

--- src/app/lib/action.ts
+++ src/app/lib/action.ts
@@ -1,77 +1,164 @@
 "use server";
 import { connectDB } from "./mongoose";
-import { User } from "../model/User";
 import { auth } from "../lib/auth";
 import { headers } from "next/dist/server/request/headers";
 // import { redirect } from "next/dist/server/api-utils";\
 import { ChargingStation } from "../model/ChargingStation";
+import z from "zod";
+import { redirect } from "next/navigation";
+import { formState } from "@/app/Types";
 
-export async function CreateUser(formData: FormData) {
-    await connectDB();
+export async function SignupUser(prevState: formState, formData: FormData): Promise<formState> {
 
-    await User.create({
-        username: formData.get("username") as string,
+    const user = z.object({
+        name: z.string().min(3).max(30).regex(/[A-Za-z][A-Za-z0-9\-]*/),
+        email: z.email(),
+        password: z.string().min(4),
+    });
+
+    const result = user.safeParse({
+        name: formData.get("username") as string,
         email: formData.get("email") as string,
         password: formData.get("password") as string,
     });
+
+    if (!result.success) {
+        console.log(result.error);
+        return {
+            status: "failed",
+            msg: "Invalid input"
+        };
+    }
+
+    const { name, email, password } = result.data;
+
+    try {
+        await auth.api.signUpEmail({
+            body: {
+                name, // required
+                email, // required
+                password, // required
+            },
+        });
+        return {
+            status: "success",
+            msg: "User Registered Successfully"
+        };
+    } catch (error) {
+        console.error("Error during sign up:", error);
+        return {
+            status: "failed",
+            msg: "Error during sign up"
+        };
+    }
 }
 
-export async function SignupUser(formData: FormData) {
-    const name = formData.get("username") as string;
-    const password = formData.get("password") as string;
-    const email = formData.get("email") as string;
+export async function LoginUser(prevState: formState, formData: FormData): Promise<formState> {
 
-    await auth.api.signUpEmail({
-        body: {
-            name, // required
-            email, // required
-            password, // required
-            callbackURL: "http://localhost:3000/",
-        },
+    const user = z.object({
+        email: z.email(),
+        password: z.string().min(4),
     });
 
-    // redirect();
-}
-
-export async function LoginUser(formData: FormData) {
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-
-    await auth.api.signInEmail({
-        body: {
-            email, // required
-            password, // required
-            rememberMe: true,
-            callbackURL: "http://localhost:3000/",
-        },
-        // This endpoint requires session cookies.
-        headers: await headers(),
+    const result = user.safeParse({
+        email: formData.get("email") as string,
+        password: formData.get("password") as string,
     });
+    if (!result.success) {
+        console.log(result.error);
+        return {
+            status: "failed",
+            msg: "Invalid input"
+        };
+    }
+    const { email, password } = result.data;
 
+    try {
+        await auth.api.signInEmail({
+            body: {
+                email, // required
+                password, // required
+                rememberMe: true,
+            },
+            // This endpoint requires session cookies.
+            headers: await headers(),
+        });
+        return {
+            status: "success",
+            msg: "User Logged In Successfully"
+        };
+    } catch (error) {
+        console.error("Error during login:", error);
+        return {
+            status: "failed",
+            msg: "Invalid email or password"
+        };
+    }
 }
 
 export async function LogoutUser() {
-    await auth.api.signOut({
-        // This endpoint requires session cookies.
-        headers: await headers(),
-    });
+    try {
+        await auth.api.signOut({
+            headers: await headers(),
+        });
+        redirect("/");
+    } catch (error) {
+        console.error("Error during logout:", error);
+        throw error;
+    }
 }
 
-export async function CreateChargingStation(formData: FormData) {
-    await connectDB();
+export async function CreateChargingStation(prevState: formState, formData: FormData): Promise<formState> {
 
-    const session = await auth.api.getSession({
-        headers: await headers(),
+    const chargingStation = z.object({
+        stationName: z.string().optional(),
+        userLat: z.string(),
+        userLong: z.string(),
     });
 
-    await ChargingStation.create({
-        owner: session!.user.id,
-        location: {
-            type: "Point",
-            coordinates: [
-                parseFloat(formData.get("userLat") as string),
-                parseFloat(formData.get("userLong") as string),
-            ],
-        },
+    const result = chargingStation.safeParse({
+        stationName: formData.get("stationName") as string,
+        userLat: formData.get("userLat") as string,
+        userLong: formData.get("userLong") as string,
     });
+
+    if (!result.success) {
+        console.log(result.error);
+        return {
+            status: "failed",
+            msg: "Invalid input"
+        }
+    }
+
+    const { userLat, userLong, stationName } = result.data;
+
+    try {
+        await connectDB();
+
+        const session = await auth.api.getSession({
+            headers: await headers(),
+        });
+
+        await ChargingStation.create({
+            stationName: stationName,
+            owner: session!.user.id,
+            location: {
+                type: "Point",
+                coordinates: [
+                    parseFloat(userLat),
+                    parseFloat(userLong),
+                ],
+            },
+        });
+        return {
+            status: "success",
+            msg: "Charging Station Created Successfully"
+        };
+    } catch (error) {
+        console.error("Error during creating charging station:", error);
+        return {
+            status: "failed",
+            msg: "Error during creating charging station"
+        };
+    }
 }
