881b38b6efb7cfc10bfef6e2fe4e8dce1cb519f3
Parth Bhovad <parthbhovad710@gmail.com>
2025-12-10 16:34:13 +0530

    useActionState and error handling added

--- src/app/ui/Login.tsx
+++ src/app/ui/Login.tsx
@@ -1,25 +1,34 @@
 "use client";
-import { useState } from "react";
+import { useActionState } from "react";
 import EmailInput from "./EmailInput";
 import PasswordInput from "./PasswordInput";
 import Button from "./Button";
 import { LoginUser } from "../lib/action";
+import { redirect } from "next/navigation";
+import {formState} from "@/app/Types";
+
 
 function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [msg, formAction, pending] = useActionState<formState, FormData>(LoginUser, { status: "", msg: "" });
+    if (msg.status === "success") {
+        redirect("/profile");
+    }
     return (
         <fieldset className="fieldset border-base-300 rounded-box w-xs border p-4">
-            <form action={LoginUser} className="grid">
+            <form action={formAction} className="grid">
                 <div className="my-2">
-                    <EmailInput email={email} setEmail={setEmail} />
+                    <EmailInput />
                 </div>
                 <div className="my-2">
-                    <PasswordInput password={password} setPassword={setPassword} />
+                    <PasswordInput />
                 </div>
-
+                {msg.status === "failed" && (
+                    <div role="alert" className="alert alert-error alert-dash">
+                        <span>{msg.msg}</span>
+                    </div> 
+                )}
                 <div className="my-4">
-                    <Button btnName="Log In" btnType="submit" />
+                    <Button btnName="Log In" btnType="submit" loading={pending} />
                 </div>
             </form>
         </fieldset>
