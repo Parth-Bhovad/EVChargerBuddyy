0362c159a28d5044a17ed4beb1c20c8b513c1ce0
Parth Bhovad <parthbhovad710@gmail.com>
2025-12-10 16:35:12 +0530

    useActionState and better error handling added

--- src/app/ui/Signup.tsx
+++ src/app/ui/Signup.tsx
@@ -1,30 +1,38 @@
 "use client";
-import { useState } from "react";
 import UserNameInput from "./UserNameInput";
 import EmailInput from "./EmailInput";
 import PasswordInput from "./PasswordInput";
 import Button from "./Button";
 import { SignupUser } from "../lib/action";
+import { useActionState } from "react";
+import { formState } from "../Types";
+import { redirect } from "next/navigation";
+
 
 function Signup() {
-    const [username, setUsername] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [msg, formAction, pending] = useActionState<formState, FormData>(SignupUser, { status: "", msg: "" });
+    if (msg.status === "success") {
+        redirect("/profile");
+    }
     return (
         <fieldset className="fieldset border-base-300 rounded-box w-xs border p-4">
-            <form action={SignupUser} className="grid">
+            <form action={formAction} className="grid">
                 <div className="my-2">
-                    <UserNameInput username={username} setUsername={setUsername} />
+                    <UserNameInput />
                 </div>
                 <div className="my-2">
-                    <EmailInput email={email} setEmail={setEmail} />
+                    <EmailInput />
                 </div>
                 <div className="my-2">
-                    <PasswordInput password={password} setPassword={setPassword} />
+                    <PasswordInput />
                 </div>
-
+                {msg.status === "failed" && (
+                    <div role="alert" className="alert alert-error alert-dash">
+                        <span>{msg.msg}</span>
+                    </div>
+                )}
                 <div className="my-4">
-                    <Button btnName="Sign Up" btnType="submit" />
+                    <Button btnName="Sign Up" btnType="submit" loading={pending} />
                 </div>
             </form>
         </fieldset>
