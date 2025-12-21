6533423b217e8b471a81fd364115e9cec9e85f36
Parth Bhovad <parthbhovad710@gmail.com>
2025-12-21 12:05:24 +0530

    RouteSteps type added

--- src/app/Types.ts
+++ src/app/Types.ts
@@ -2,3 +2,9 @@ export interface formState {
     status: string;
     msg: string;
 }
+
+export type RouteSteps = Array<{
+    distance: number;
+    duration: number;
+    instruction: string;
+}>;
