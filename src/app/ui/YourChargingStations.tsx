c46f5aab4e15789c63ee801c4b81bb1886dc14cc
Parth Bhovad <parthbhovad710@gmail.com>
2025-12-22 15:58:38 +0530

    Small UI Improvement and type safty added

--- src/app/ui/YourChargingStations.tsx
+++ src/app/ui/YourChargingStations.tsx
@@ -1,11 +1,10 @@
+import {ChargingStationProps} from '../Types';
 
-
-function YourChargingStations({ chargingStations }) {
-    console.log(chargingStations);
+function YourChargingStations({ chargingStations }: { chargingStations: ChargingStationProps }) {
 
     return (
-        <div className="mt-8">
-            <table className="table">
+        <div className="mt-8 h-full overflow-auto rounded-box border border-base-content/5 border-black">
+            <table className="table" >
                 {/* head */}
                 <thead className="w-full text-[var(--brand-color-black)]">
                     <tr>
@@ -16,7 +15,7 @@ function YourChargingStations({ chargingStations }) {
                 </thead>
                 <tbody>
                     {chargingStations.map((station, idx) => (
-                        <tr key={idx + 1} className="list-row hover:bg-stone-200">
+                        <tr key={idx + 1} className="list-row hover:bg-stone-200 font-medium">
                             <th>{idx + 1}</th>
                             <td>{station.stationName}</td>
                             <td>{station.location.join(", ")}</td>
