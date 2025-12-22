23f4277504c9b50dd4a77851aae0265ad0c65f9b
Parth Bhovad <parthbhovad710@gmail.com>
2025-12-22 15:58:20 +0530

    Small UI Improvement and type safty added

--- src/app/your-charging-stations/page.tsx
+++ src/app/your-charging-stations/page.tsx
@@ -3,23 +3,27 @@ import { connectDB } from "../lib/mongoose";
 import YourChargingStations from "../ui/YourChargingStations";
 import { auth } from "@/app/lib/auth";
 import { headers } from "next/headers";
+import { ChargingStationProps } from "../Types";
 
 async function page() {
     const session = await auth.api.getSession({
         headers: await headers() // you need to pass the headers object.
     })
     await connectDB();
+    
     const rawData = await ChargingStation.find({ owner: session?.user?.id });
-    const chargingStations = rawData.map(station => {
+    
+    const chargingStations: ChargingStationProps = rawData.map(station => {
         return {
             stationName: station.stationName,
             location: station.location.coordinates,
             owner: station.owner
         }
     })
+    
     return (
         <main className="h-screen">
-            <h1 className="text-2xl font-semibold mb-3 text-[var(--brand-color-black)] text-center">Your Charging Stations</h1>
+            <h1 className="text-2xl font-semibold my-3 text-[var(--brand-color-black)] text-center">Your Charging Stations</h1>
             <YourChargingStations chargingStations={chargingStations} />
         </main>
     );
