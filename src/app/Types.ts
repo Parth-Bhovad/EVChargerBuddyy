9c3d6b77b16dcba41aec541a059c71058717a543
Parth Bhovad <parthbhovad710@gmail.com>
2025-12-23 21:00:26 +0530

    new types added

--- src/app/Types.ts
+++ src/app/Types.ts
@@ -14,3 +14,38 @@ export type ChargingStationProps = Array<{
     location: [number, number];
     owner: string;
 }>;
+
+export type Stations = Array<{
+    _id: string;
+    location: {
+        type: string;
+        coordinates: [number, number];
+    };
+    owner: string;
+    stationName: string;
+}>;
+
+export type GetRouteResponse =
+    | {
+        status: "success";
+        data:{
+            steps: RouteSteps;
+            duration: number;
+            distance: number;
+            coordinates: [number, number][];
+        }
+    }
+    | {
+        status: "error";
+        error: string;
+    };
+
+export type NearByStationsResponse =
+    | {
+        status: "success";
+        stations: Stations;
+    }
+    | {
+        status: "error";
+        error: string;
+    }
