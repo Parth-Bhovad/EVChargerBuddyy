26589d465ac829f6bf84cfd50819ce4bc016e750
Parth Bhovad <parthbhovad710@gmail.com>
2025-12-23 20:58:20 +0530

    return type added in toORSCoord function

--- src/app/lib/helper.ts
+++ src/app/lib/helper.ts
@@ -1,4 +1,4 @@
-export const toORSCoord = (latlng:[number, number]) => {
+export const toORSCoord = (latlng:[number, number]) : [number, number] => {
     console.log(latlng);
     
     return [latlng[1], latlng[0]];
