6d8f04908f909ff12fa07cb5ad0678e15a5e66bd
Parth Bhovad <parthbhovad710@gmail.com>
2025-12-24 23:04:21 +0530

    Signed and without signed up users Navbar added

--- src/app/ui/Navbar.tsx
+++ src/app/ui/Navbar.tsx
@@ -1,6 +1,50 @@
+import { auth } from "@/app/lib/auth"; // path to your Better Auth server instance
+import { headers } from "next/headers";
 import Link from "next/link";
 
-function Navbar() {
+async function Navbar() {
+    const session = await auth.api.getSession({
+        headers: await headers() // you need to pass the headers object.
+    })
+
+    if (!session) {
+        return (
+            <div className="drawer bg-[var(--brand-color-black)] text-[var(--brand-color-white)] z-50 sticky top-0">
+                <div className="drawer-content flex flex-col">
+                    {/* Navbar */}
+                    <div className="navbar bg-base-300 w-full">
+                        <div className="mx-2 flex-1 px-2 font-bold text-2xl"><Link href="/">EVChargerBuddy</Link></div>
+                        <div className="hidden flex-none lg:block">
+                            <ul className="menu menu-horizontal">
+                                <li><Link href="/signup" className="font-bold">Sign Up</Link></li>
+                                <li><Link href="/login" className="font-bold">Log In</Link></li>
+                            </ul>
+                        </div>
+                        <div className="flex-none lg:hidden">
+                            <div className="drawer drawer-end">
+                                <input id="my-drawer-5" type="checkbox" className="drawer-toggle" />
+                                <div className="drawer-content bg-transparent">
+                                    {/* Page content here */}
+                                    <label htmlFor="my-drawer-5" className="drawer-button btn bg-transparent">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path> </svg>
+                                    </label>
+                                </div>
+                                <div className="drawer-side">
+                                    <label htmlFor="my-drawer-5" aria-label="close sidebar" className="drawer-overlay"></label>
+                                    <ul className="menu bg-[var(--brand-color-black)] text-[var(--brand-color-white)] min-h-full w-80 p-4">
+                                        {/* Sidebar content here */}
+                                        <li><Link href="/signup" className="font-bold">Sign Up</Link></li>
+                                        <li><Link href="/login" className="font-bold">Log In</Link></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div className="drawer bg-[var(--brand-color-black)] text-[var(--brand-color-white)] z-50 sticky top-0">
             <div className="drawer-content flex flex-col">
