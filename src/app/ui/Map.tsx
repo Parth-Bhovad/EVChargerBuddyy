c1af2ac721127a06cfe34ddc2e77445d82950469
Parth Bhovad <parthbhovad710@gmail.com>
2026-01-06 22:14:59 +0530

    local file path added instead of cdn

--- src/app/ui/Map.tsx
+++ src/app/ui/Map.tsx
@@ -38,12 +38,9 @@ function LocationMarker() {
 }
 
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
-  iconUrl:
-    'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
-  shadowUrl:
-    'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+  iconRetinaUrl: "/marker-icon-2x.png",
+  iconUrl:"/marker-icon.png",
+  shadowUrl:"/marker-shadow.png",
 });
 
 const LeafletMap = () => {
