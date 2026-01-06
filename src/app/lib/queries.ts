87608b0a469ac9e390a212cc2eec54cf6269025f
Parth Bhovad <parthbhovad710@gmail.com>
2026-01-06 22:12:38 +0530

    queries file added for queries

---
+++ src/app/lib/queries.ts
@@ -0,0 +1,15 @@
+import { ChargingStation } from "@/app/model/ChargingStation";
+import { connectDB } from "@/app/lib/mongoose";
+import { ChargingStationProps } from "@/app/Types";
+
+export async function getChargingStationsByOwner(userId: string): Promise<ChargingStationProps> {
+  await connectDB();
+
+  const rawData = await ChargingStation.find({ owner: userId }).lean();
+
+  return rawData.map(station => ({
+    stationName: station.stationName,
+    location: station.location.coordinates,
+    owner: station.owner,
+  }));
+}
