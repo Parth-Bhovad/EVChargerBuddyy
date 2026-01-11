3569fb0ed02cc7a15fd94baff9aa3b82fe8c6977
Parth Bhovad <parthbhovad710@gmail.com>
2026-01-11 14:42:05 +0530

    loading states added in context for better UX.

--- src/app/context/UserLocationContext.tsx
+++ src/app/context/UserLocationContext.tsx
@@ -5,6 +5,8 @@ import { createContext, useState, useContext } from 'react';
 const UserLocationContext = createContext<{
     userLocation: [number, number] | null;
     setUserLocation: React.Dispatch<React.SetStateAction<[number, number] | null>>;
+    gettingUserLocation: boolean;
+    setGettingUserLocation: React.Dispatch<React.SetStateAction<boolean>>;
 } | null>(null);
 
 export const useUserLocationContext = () => {
@@ -16,9 +18,10 @@ export const useUserLocationContext = () => {
 }
 
 function UserLocationProvider({ children }: { children: React.ReactNode }) {
-    const [userLocation, setUserLocation] = useState<[number, number] | null>(null)
+    const [userLocation, setUserLocation] = useState<[number, number] | null>(null);
+    const [gettingUserLocation, setGettingUserLocation] = useState<boolean>(false);
     return ( 
-        <UserLocationContext.Provider value={{ userLocation, setUserLocation }}>
+        <UserLocationContext.Provider value={{ userLocation, setUserLocation, gettingUserLocation, setGettingUserLocation }}>
             {children}
         </UserLocationContext.Provider>
      );
