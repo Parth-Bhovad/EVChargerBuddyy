6cfef3998b9b0d5a9aa103631629f2d57d3fce0e
Parth Bhovad <parthbhovad710@gmail.com>
2026-01-18 16:53:46 +0530

    using useTravelDistanceAndTimeContext and added ShowRouteInfo in useEffect

--- src/app/hooks/useMap.ts
+++ src/app/hooks/useMap.ts
@@ -4,28 +4,28 @@ import { toORSCoord } from '../lib/helper';
 import { useUserDestinationContext } from '../context/UserDestinationContext';
 import { useRouteStepsContext } from '../context/RouteStepsContext';
 import { GetRouteResponse } from '../Types';
+import { useShowRouteInfoContext } from '../context/ShowRouteInfoContext';
+import { useTravelDistanceAndTimeContext } from '../context/TravelDistanceAndTimeContext';
 
 function useMap() {
     const { userLocation } = useUserLocationContext();
     const { setRouteSteps } = useRouteStepsContext();
+    const { ShowRouteInfo } = useShowRouteInfoContext();
+    const { stationDistance, setStationDistance, destinationTime, setDestinationTime } = useTravelDistanceAndTimeContext();
     const [polyline, setPolyline] = useState<Array<[number, number]>>([
         [49.41461, 8.681495],
         [49.420318, 8.687872]
     ]);
-    //station distance in KM
-    const [stationDistance, setStationDistance] = useState<number | null>(null);
-    //time needed to reach station in minutes
-    const [destinationTime, setDestinationTime] = useState<number | null>(null);
 
     const { userDestination } = useUserDestinationContext();
 
     useEffect(() => {
         //Return if userLocation or destination is null
-        if (!userLocation || !userDestination) return;
+        if ((!userLocation || !userDestination) || ShowRouteInfo) return;
         const callAPI = async () => {
             const response = await fetch(`/api/get-route?start=${toORSCoord(userLocation!).join(',')}&end=${toORSCoord(userDestination!).join(',')}&preference=shortest`);
             const data: GetRouteResponse = await response.json();
-
+            console.log("Route data:", data);
             if (data.status === "error") {
                 console.error("Error fetching route:", data.error);
                 return;
@@ -37,7 +37,7 @@ function useMap() {
             setRouteSteps(steps);
         }
         callAPI();
-    }, [userDestination, userLocation, setRouteSteps]);
+    }, [userDestination, userLocation, setRouteSteps, ShowRouteInfo, setStationDistance, setDestinationTime]);
     return ({ polyline, stationDistance, destinationTime });
 }
 
